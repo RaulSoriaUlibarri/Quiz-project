caacb420127105aff4badba63f8c76761dc88b70
Ruls <soria.developer@gmail.com>
2026-06-10 16:47:28 -0600

    feat(quizz): add shuffle answers logic

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 const Quizz = () => {
   const [userAnswers, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevAnswers) => {
@@ -12,12 +14,33 @@ const Quizz = () => {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Quiz completed img" />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const answers = [...QUESTIONS[activeQuestionIndex].answers];
+
+  function handleShuffleAnswers(arr) {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr;
+  }
+
+  const suffledAnswers = handleShuffleAnswers(answers);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+          {suffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
