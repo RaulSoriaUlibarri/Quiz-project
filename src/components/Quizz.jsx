e724349c7fafedb422c79b4d9c88ff0350360a03
Ruls <soria.developer@gmail.com>
2026-06-10 15:41:46 -0600

    feat: show answers and register them on click

--- src/components/Quizz.jsx
+++ src/components/Quizz.jsx
@@ -6,14 +6,26 @@ const Quizz = () => {
 
   const activeQuestionIndex = userAnswers.length;
 
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevAnswers) => {
+      return [...prevAnswers, selectedAnswer];
+    });
+  }
+
   return (
-    <div id="question">
-      <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
-      <ul id="answers">
-        {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
-          <li>{answer}</li>
-        ))}
-      </ul>
+    <div id="quiz">
+      <div id="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 };
