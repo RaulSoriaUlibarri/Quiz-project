4943b1eb04330aa4453d2b8406e848eb01966c52
Ruls <soria.developer@gmail.com>
2026-06-10 23:59:38 -0600

    feat: separate logic into Question component

---
+++ src/components/Question.jsx
@@ -0,0 +1,25 @@
+import { QuestionTimer, Answers } from "./index";
+
+const Question = ({
+  questionText,
+  answers,
+  onSelectAnswer,
+  selectedAnswer,
+  answerState,
+  onSkipAnswer,
+}) => {
+  return (
+    <div id="question">
+      <QuestionTimer timeout={10000} onTimeout={onSkipAnswer} />
+      <h2>{questionText}</h2>
+      <Answers
+        answers={answers}
+        selectedAnswer={selectedAnswer}
+        answerState={answerState}
+        onSelect={onSelectAnswer}
+      />
+    </div>
+  );
+};
+
+export default Question;
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export { default as Header } from "./Header";
 export { default as Quizz } from "./Quizz";
 export { default as QuestionTimer } from "./QuestionTimer";
+export { default as Answers } from "./Answers";
+export { default as Question } from "./Question";
